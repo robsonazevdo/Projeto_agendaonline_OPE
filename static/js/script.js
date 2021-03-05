96a95f70c92045c2536a7b833ae1d69fcb12bc2e
robson <kassapete@hotmail.com>
2021-03-05 12:46:03 -0300

    Primeiro commit da agenda

--- static/js/script.js
+++ static/js/script.js
@@ -4,36 +4,6 @@ const btn = document.querySelector(".btn-cadastro");
 var form = document.querySelector(".form");
 var btnForm = document.querySelector(".btn-fechar");
 
-let time = 3000,
-    currentImageIndex = 0,
-    images = document.querySelectorAll("#slider img")
-max = images.length;
-
-//banner rotativo//
-
-function nextImage() {
-
-    images[currentImageIndex].classList.remove("selected")
-
-    currentImageIndex++
-
-    if (currentImageIndex >= max)
-        currentImageIndex = 0
-
-    images[currentImageIndex].classList.add("selected")
-
-}
-
-function start() {
-    setInterval(() => {
-        nextImage()
-    }, time)
-}
-
-window.addEventListener("load", start);
-//fim banner rotativo//
-
-
 function navFix() {
     if (window.pageYOffset >= 200) {
 
@@ -58,3 +28,54 @@ function toggle(event) {
 
 btn.addEventListener("click", toggle, false);
 btnForm.addEventListener("click", toggle, false);
+
+//banner rotativo//
+let time = 3000,
+
+    currentImageIndex = 0,
+    images = document.querySelectorAll("#slider img")
+max = images.length;
+
+
+function nextImage() {
+
+    images[currentImageIndex].classList.remove("selected")
+
+    currentImageIndex++
+
+    if (currentImageIndex >= max)
+        currentImageIndex = 0
+
+    images[currentImageIndex].classList.add("selected")
+
+}
+
+function start() {
+    setInterval(() => {
+        nextImage()
+    }, time)
+}
+
+window.addEventListener("load", start);
+//fim banner rotativo//
+
+
+
+//inicio calendario// 
+
+let monthNames = ['Janeiro', 'Fevereiro', 'Março', 'April', 'Maio', 'Junho', 'Julio', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+
+let currentDate = new Date();
+let currentDay = currentDate.getDate();
+let monthNumber = currentDate.getMonth();
+let currentYear = currentDate.getFullYear();
+
+let dates = document.getElementById('dates');
+var month = document.getElementById('month').innerHTML = monthNames[monthNumber];
+let year = document.getElementById('year').innerHTML = currentYear.toString();
+
+let prevMonthDOM = document.getElementById('prev-month');
+let nextMonthDOM = document.getElementById('next-month');
+
+
+//fim calendario//
